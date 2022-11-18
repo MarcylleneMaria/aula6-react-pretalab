de1aba62b1a2467f6d4e2bb0acce5a2e1f71f03d
MarcylleneMaria <marcyllenemaria@id.uff.br>
2022-11-18 16:25:24 -0300

    subindo codigo aula 6 ainda nao finalizado

---
+++ src/data/profile-data.js
@@ -0,0 +1,6 @@
+export const informacao = {
+    nome: "Marcyllene",
+    apelido: "Marcy",
+    foto: "https://media-exp1.licdn.com/dms/image/C4D03AQF5PnXl2ta_iw/profile-displayphoto-shrink_800_800/0/1651160435078?e=2147483647&v=beta&t=suZz-YjCnMh91-4JHkyiQ2pRsOr5_YzbCfuyFuDGa-w",
+    descricao: 'Olá, eu sou Marcyllene. Sou estudante de Ciência Ambiental na UFF e estagiária em Sensoriamento Remoto na SCCON Geoespatial'
+}
